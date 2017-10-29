5f7b399084667de3321cefa4180e8a21bda988f9
YellowBeard <meet11061997@gmail.com>
2017-10-29 16:13:15 +0530

    char file added

--- 03/include/Joint.hpp
+++ 03/include/Joint.hpp
@@ -1,12 +1,15 @@
 #ifndef _JOINT_HPP_
 #define _JOINT_HPP_
 
+#define HINGE_JOINT 0
+#define BALL_JOINT 1
+
 #include "Include.hpp"
 #include "Part.hpp"
 
 class Joint
 {
-	glm::vec4 attach_point;
+	glm::mat4 init_transform;
 	class Part *child, *parent;
 
 public:
@@ -18,17 +21,16 @@ public:
 class HingeJoint:Joint
 {
 	GLfloat rot_angle, min_rot_angle, max_rot_angle;
-	glm::mat4 init_transform;
 	glm::vec3 axis;
 
 public:
 	HingeJoint();
+	HingeJoint(GLfloat)
 	glm::mat4 getT();
 };
 
 class BallJoint:Joint
 {
-	glm::mat4 init_transform;
 	glm::vec3 axis;
 	glm::mat4 rot_matrix;
 
