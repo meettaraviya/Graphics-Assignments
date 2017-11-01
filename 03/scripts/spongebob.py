7fcfcdb46bc46f9e9e23d1a806a44802c7a319a7
rajatk96 <rajatk96@github.com>
2017-11-01 23:16:00 +0530

    final spongebob

--- 03/scripts/spongebob.py
+++ 03/scripts/spongebob.py
@@ -32,52 +32,54 @@ def cuboid(arg):
 		# v.append([])
 
 	return v
-
+x = {}
 def toFile(v, name):
 	f = open(name, "a")
+	if name not in x:
+		f.write("0\n")
+		x[name] = 1
 	for vi in v:
 		f.write(" ".join([str(i) for i in vi])+"\n")
 	f.close()
 
 ##front part
-toFile(cuboid(["x",0,-50,0,30,60,10,0,0,1]),"front.raw")
-toFile(cuboid(["x",0,-85,0,30,10,10,1,0,0]),"front.raw")
-toFile(cuboid(["x",0,-100,0,30,20,10,0,1,0]),"front.raw")
-toFile(cuboid(["x",7.5,-112.5,0,10,5,10,0,1,1]),"front.raw")
-toFile(cuboid(["x",-7.5,-112.5,0,10,5,10,0,1,1]),"front.raw")
+toFile(cuboid(["x",0,-50,0,30,60,10,0xf7/256,0xff/256,0x62/256]),"front.raw")
+toFile(cuboid(["x",0,-85,0,30,10,10,1,1,1]),"front.raw")
+toFile(cuboid(["x",0,-100,0,30,20,10,0.8125,0x65/256,0x0d/256]),"front.raw")
+toFile(cuboid(["x",7.5,-112.5,0,10,5,10,0.8125,0x65/256,0x0d/256]),"front.raw")
+toFile(cuboid(["x",-7.5,-112.5,0,10,5,10,0.8125,0x65/256,0x0d/256]),"front.raw")
 
 
 ##right hand
-toFile(cuboid(["x",20,-50,0,10,10,2.5,1,0,1]),"right_hand_low.raw")
-toFile(cuboid(["x",35,-50,0,20,5,2.5,0.5,0.5,1]),"right_hand_low.raw")
+toFile(cuboid(["x",20,-50,0,10,10,2.5,1,1,1]),"right_hand_low.raw")
+toFile(cuboid(["x",35,-50,0,20,5,2.5,0xf7/256,0xff/256,0x62/256]),"right_hand_low.raw")
 
-toFile(cuboid(["x",55,-50,0,20,5,2.5,0.3,0,1]),"right_hand_high.raw")
-toFile(cuboid(["x",55,-50,0,20,5,2.5,0.3,0,1]),"right_hand_high.raw")
-
-toFile(cuboid(["x",67.5,-47.5,0,5,15,2.5,0.3,0,1]),"right_hand_high.raw")
-toFile(cuboid(["x",70,-50,0,10,10,2.5,0.3,0,1]),"right_hand_high.raw")
+toFile(cuboid(["x",55,-50,0,20,5,2.5,0xf7/256,0xff/256,0x62/256]),"right_hand_high.raw")
+toFile(cuboid(["x",55,-50,0,20,5,2.5,0xf7/256,0xff/256,0x62/256]),"right_hand_high.raw")
+toFile(cuboid(["x",67.5,-47.5,0,5,15,2.5,0xf7/256,0xff/256,0x62/256]),"right_hand_high.raw")
+toFile(cuboid(["x",70,-50,0,10,10,2.5,0xf7/256,0xff/256,0x62/256]),"right_hand_high.raw")
 
 
 ##left hand
-toFile(cuboid(["x",-20,-50,0,10,10,2.5,1,0,1]),"left_hand_low.raw")
-toFile(cuboid(["x",-35,-50,0,20,5,2.5,0.5,0.5,1]),"left_hand_low.raw")
+toFile(cuboid(["x",-20,-50,0,10,10,2.5,1,1,1]),"left_hand_low.raw")
+toFile(cuboid(["x",-35,-50,0,20,5,2.5,0xf7/256,0xff/256,0x62/256]),"left_hand_low.raw")
 
-toFile(cuboid(["x",-55,-50,0,20,5,2.5,0.3,0,1]),"left_hand_high.raw")
-toFile(cuboid(["x",-55,-50,0,20,5,2.5,0.3,0,1]),"left_hand_high.raw")
+toFile(cuboid(["x",-55,-50,0,20,5,2.5,0xf7/256,0xff/256,0x62/256]),"left_hand_high.raw")
+toFile(cuboid(["x",-55,-50,0,20,5,2.5,0xf7/256,0xff/256,0x62/256]),"left_hand_high.raw")
 
-toFile(cuboid(["x",-67.5,-47.5,0,5,15,2.5,0.3,0,1]),"left_hand_high.raw")
-toFile(cuboid(["x",-70,-50,0,10,10,2.5,0.3,0,1]),"left_hand_high.raw")
+toFile(cuboid(["x",-67.5,-47.5,0,5,15,2.5,0xf7/256,0xff/256,0x62/256]),"left_hand_high.raw")
+toFile(cuboid(["x",-70,-50,0,10,10,2.5,0xf7/256,0xff/256,0x62/256]),"left_hand_high.raw")
 
 ##right leg
-toFile(cuboid(["x",7.5,-120,0,5,10,2.5,0.3,0,1]),"right_leg_high.raw")
-toFile(cuboid(["x",7.5,-130,0,5,10,2.5,0.3,0,1]),"right_leg_low.raw")
-toFile(cuboid(["x",7.5,-140,0,10,10,10,0.3,0,1]),"right_leg_low.raw")
+toFile(cuboid(["x",7.5,-120,0,5,10,2.5,0xf7/256,0xff/256,0x62/256]),"right_leg_high.raw")
+toFile(cuboid(["x",7.5,-130,0,5,10,2.5,0xf7/256,0xff/256,0x62/256]),"right_leg_low.raw")
+toFile(cuboid(["x",7.5,-140,0,10,10,10,0,0,0]),"right_leg_low.raw")
 
 
 ##left leg
-toFile(cuboid(["x",-7.5,-120,0,5,10,2.5,0.3,0,1]),"left_leg_high.raw")
-toFile(cuboid(["x",-7.5,-130,0,5,10,2.5,0.3,0,1]),"left_leg_low.raw")
-toFile(cuboid(["x",-7.5,-140,0,10,10,10,0.3,0,1]),"left_leg_low.raw")
+toFile(cuboid(["x",-7.5,-120,0,5,10,2.5,0xf7/256,0xff/256,0x62/256]),"left_leg_high.raw")
+toFile(cuboid(["x",-7.5,-130,0,5,10,2.5,0xf7/256,0xff/256,0x62/256]),"left_leg_low.raw")
+toFile(cuboid(["x",-7.5,-140,0,10,10,10,0,0,0]),"left_leg_low.raw")
 
 
 
