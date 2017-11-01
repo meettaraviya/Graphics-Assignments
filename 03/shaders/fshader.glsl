2e431acc70d9d67afb67ab1751bb277961643e38
YellowBeard <meet11061997@gmail.com>
2017-11-01 18:32:27 +0530

    face

--- 03/shaders/fshader.glsl
+++ 03/shaders/fshader.glsl
@@ -9,8 +9,8 @@ uniform sampler2D t_sampler;
 void main ()
 {
   // frag_color = vec4(UV[0],UV[1],0,1);
-  // frag_color = texture( t_sampler, UV ).rgba;
-  frag_color = vec4(UV,0.0, 1.0);
+  frag_color = texture( t_sampler, UV ).rgba;
+  // frag_color = vec4(UV,0.0, 1.0);
   // vec3 c = texture( t_sampler, vec2(0,0.5) ).rgb;
   // frag_color = vec4(c[0], c[1], c[2], 1.0);
   // frag_color = vec4(color[0], UV[1], UV[0], 0.0);
--- 03/src/main.cpp
+++ 03/src/main.cpp
@@ -116,10 +116,10 @@ GLuint loadTexture( const char * filename)
     glGenTextures( 1, &texture );
     glBindTexture( GL_TEXTURE_2D, texture );
     
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
    
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
